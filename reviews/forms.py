61ac6983156b35c11659efb0ef4f15950b91a168
Eric Todasco <erictodasco@me.com>
2025-05-13 12:13:01 -0400

    added styles.css to review.html form class

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your Name", max_length=50, error_messages={
+        "required": "Name can not be empty!",
+        "max_length": "Max character length must be shorter than 50 characters!"
+    })
