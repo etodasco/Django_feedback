803e776af875804484fc1315752e20cf5d74767d
Eric Todasco <erictodasco@me.com>
2025-05-16 11:27:25 -0400

    updated views.py

--- reviews/views.py
+++ reviews/views.py
@@ -3,30 +3,18 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
@@ -44,10 +32,3 @@ class ReviewsListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     review_id = kwargs["id"]
-    #     selected_review = Review.objects.get(pk=review_id)
-    #     context["review"] = selected_review
-    #     return context
